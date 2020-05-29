d019c65b687789603c18a2189adba292a797c87e
takahash1 <45808212+takahash1@users.noreply.github.com>
2020-05-28 21:43:30 -0300

    Add files via upload

---
+++ JavaScript/Exercício 1/script.js
@@ -0,0 +1,16 @@
+/* Ex2 e 3 Função criada em um ficheiro externo que mostra uma mensagem*/
+
+function alertar(){
+	alert("AIA");
+	}
+
+function apagar(){
+	document.getElementById("myImg").src = "acesa.jpg";
+   } 	
+function acender(){
+	document.getElementByID("myImg").src = "apagada.jpg";
+   }
+function imprimenome(){
+	document.getElementById("name")
+	alert("nomecompleto");
+}
