08dc52b80eb35b38277d1b61522e0944006e569b
takahash1 <45808212+takahash1@users.noreply.github.com>
2020-05-28 21:41:52 -0300

    Add files via upload

---
+++ JavaScript/Exercício Obj e API/script.js
@@ -0,0 +1,9 @@
+var receita = {
+    titulo: "Brigadeiro",
+    porcoes: 10,
+    ingredientes: ["Leite condensado", "Achocolatado em pó", "Manteiga"]
+}
+
+console.log(receita.titulo);
+console.log(receita.porcoes);
+console.log(receita.ingredientes);
