de17a056b5625da7a40dbf6ff033867fcb9ce0c7
takahash1 <45808212+takahash1@users.noreply.github.com>
2020-05-31 19:55:51 -0300

    Delete script.js

--- JavaScript/Exercício Obj e API/script.js
+++
@@ -1,9 +0,0 @@
-var receita = {
-    titulo: "Brigadeiro",
-    porcoes: 10,
-    ingredientes: ["Leite condensado", "Achocolatado em pó", "Manteiga"]
-}
-
-console.log(receita.titulo);
-console.log(receita.porcoes);
-console.log(receita.ingredientes);
