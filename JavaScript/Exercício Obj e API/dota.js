1c23f63ced15271d3709871912246fc1df0d16cb
takahash1 <45808212+takahash1@users.noreply.github.com>
2020-06-03 17:23:24 -0300

    Add files via upload

--- JavaScript/Exercício Obj e API/dota.js
+++ JavaScript/Exercício Obj e API/dota.js
@@ -1,10 +1,20 @@
-let tabela = document.getElementById('')
+let data = document.getElementById('data')
+let inf1 = document.getElementById('inf1')
+let inf2 = document.getElementById('inf2')	
+let inf3 = document.getElementById('inf3')
+let inf4 = document.getElementById('inf4')
+let inf5 = document.getElementById('inf5')
 
 fetch('https://api.opendota.com/api/matches/4973348952')
 .then(
 	response => response.json()
 	)
 .then(data=>{
-	console.log(data)
-	})	
+		data.innerHTML = data.match_id;
+		inf1.innerHTML = data.radiant_team.name;
+		inf2.innerHTML = data.dire_team.name;
+		inf3.innerHTML = (data.duration)/60;
+		inf4.innerHTML = data.radiant_win;
+	}
+)	
 .catch(error => console.error("Algo deu errado para carregar sua API: " + error.message))
