71d489aaa9d39e0a55b95ad8df43535e9030bb9f
takahash1 <45808212+takahash1@users.noreply.github.com>
2020-06-09 20:06:40 -0300

    Add files via upload

--- JavaScript/Exercício Obj e API/dota.js
+++ JavaScript/Exercício Obj e API/dota.js
@@ -1,20 +1,33 @@
-let data = document.getElementById('data')
-let inf1 = document.getElementById('inf1')
-let inf2 = document.getElementById('inf2')	
-let inf3 = document.getElementById('inf3')
-let inf4 = document.getElementById('inf4')
-let inf5 = document.getElementById('inf5')
+	let tabela = document.getElementById('tabela-dota');
+	let linhaid = document.getElementById('linha-id');
+	let linhatimerad = document.getElementById('linha-time-rad');	
+	let linhatimedire = document.getElementById('linha-time-dire');
+	let linhaduracao = document.getElementById('linha-duracao');
+	let linhavenc = document.getElementById('linha-venc');
 
 fetch('https://api.opendota.com/api/matches/4973348952')
 .then(
 	response => response.json()
 	)
-.then(data=>{
-		data.innerHTML = data.match_id;
-		inf1.innerHTML = data.radiant_team.name;
-		inf2.innerHTML = data.dire_team.name;
-		inf3.innerHTML = (data.duration)/60;
-		inf4.innerHTML = data.radiant_win;
+.then( 
+	data => {
+		console.log(data);		
+		data.forEach( data => {
+
+			let linha = tabela.insertRow(-1);
+			let linhaid = linha.insertCel(0)
+			let linhatimerad = linha.insertCel(1);
+			let linhatimedire = linha.insertCel(2);
+			let linhaduracao = linha.insertCel(3);
+			let linhavenc = linha.insertCel(4);
+
+			linhaid.innerHTML = data.match_id;
+			linhatimerad.innerHTML = data.radiant_team.tag;
+			linhatimedire.innerHTML = data.dire_team.tag;
+			linhaduracao.innerHTML = data.duration;
+			linhavenc.innerHTML = data.radiant_win;
+			}
+		)
 	}
-)	
+)
 .catch(error => console.error("Algo deu errado para carregar sua API: " + error.message))
